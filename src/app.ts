8c79f6f0cba4737b1d5c72dec5eb693817a0b80d
lukastff <lucastavaresffr@gmail.com>
2025-10-26 17:14:47 -0300

    feat: implement exception handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
+import { errorHandling } from "./middlewares/errorHandling";
+import { AppError } from "./utils/AppError";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello Bin"));
+app.get("/", (req, res) => {
+  throw new AppError("Socorro deus");
+
+  res.send("Hello Bin");
+});
+
+app.use(errorHandling);
 
 export { app };
