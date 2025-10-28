1d3729d73e353a682665e72f09116f6ccb6f3544
lukastff <lucastavaresffr@gmail.com>
2025-10-27 22:02:12 -0300

    feat: implement user registration with password hashing

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import { UserRole } from "@prisma/client";
+import { prisma } from "@/database/prisma";
 import z from "zod";
+import { AppError } from "@/utils/AppError";
+import { hash } from "bcrypt";
 
 class UsersController {
   async create(req: Request, res: Response, next: NextFunction) {
@@ -21,7 +24,24 @@ class UsersController {
 
     const { name, email, password, role } = bodySchema.parse(req.body);
 
-    res.send({ message: name, email, password, role });
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("An account with this email already exists.");
+    }
+
+    const hashedPasword = await hash(password, 8);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPasword,
+        role,
+      },
+    });
+
+    res.status(201).json();
   }
 }
 
