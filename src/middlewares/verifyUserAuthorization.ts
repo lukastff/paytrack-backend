c465b85a2921d9cc3ad937727352dce80ee9ab13
lukastff <lucastavaresffr@gmail.com>
2025-11-02 14:43:37 -0300

    feat: implement authorization middleware

---
+++ src/middlewares/verifyUserAuthorization.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+import { AppError } from "@/utils/AppError";
+
+function verifyUserAuthorization(role: string[]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user || !role.includes(req.user.role)) {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    return next();
+  };
+}
+
+export { verifyUserAuthorization };
--- src/routes/refundsRoutes.ts
+++ src/routes/refundsRoutes.ts
@@ -1,9 +1,14 @@
 import { RefundsController } from "@/controllers/refundsController";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 import { Router } from "express";
 
 const refundsRoutes = Router();
 const refundsController = new RefundsController();
 
-refundsRoutes.post("/", refundsController.create);
+refundsRoutes.post(
+  "/",
+  verifyUserAuthorization(["employee"]),
+  refundsController.create
+);
 
 export { refundsRoutes };
