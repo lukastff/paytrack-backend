f919be92bde626f873cab53d497d067ace0205fb
lukastff <lucastavaresffr@gmail.com>
2025-11-02 11:38:26 -0300

    feat: implement ensure-authenticated middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,16 @@ import { Router } from "express";
 import { usersRoutes } from "./usersRoutes";
 import { sessionsRoutes } from "./sessionsRoutes";
 import { refundsRoutes } from "./refundsRoutes";
+import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
 
 const routes = Router();
 
+// Public Routes
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
+
+// Private Routes
+routes.use(ensureAuthenticated);
 routes.use("/refunds", refundsRoutes);
 
 export { routes };
