b080483eb61420714660794091bbab6a9ad7baae
lukastff <lucastavaresffr@gmail.com>
2025-11-25 11:18:25 -0300

    feat: implement temporary file validation

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -5,7 +5,8 @@ import crypto from "node:crypto";
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
 const UPLOADS_FOLDER = path.relative(TMP_FOLDER, "uploads");
 
-const MAX_FILE_SIZE = 1024 * 1024 * 1;
+const MAX_SIZE = 3;
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
 const MULTER = {
@@ -24,6 +25,7 @@ export default {
   TMP_FOLDER,
   UPLOADS_FOLDER,
   MULTER,
+  MAX_SIZE,
   MAX_FILE_SIZE,
   ACCEPTED_IMAGE_TYPES,
 };
--- src/controllers/uploadsController.ts
+++ src/controllers/uploadsController.ts
@@ -1,8 +1,37 @@
 import { Request, Response, NextFunction } from "express";
+import z from "zod";
+
+import uploadConfig from "@/configs/upload";
 
 class UploadsController {
   async create(req: Request, res: Response, next: NextFunction) {
-    res.json({ message: "hello moto" });
+    try {
+      const fileSchema = z
+        .object({
+          filename: z.string().min(1, "This file is required."),
+          mimetype: z
+            .string()
+            .refine(
+              (type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type),
+              `Invalid file format. Allowed formats: ${uploadConfig.ACCEPTED_IMAGE_TYPES}`
+            ),
+          size: z
+            .number()
+            .positive()
+            .refine(
+              (size) => uploadConfig.MAX_FILE_SIZE,
+              `The file exceeds the maximum size of ${uploadConfig.MAX_SIZE}MB`
+            ),
+        })
+        .passthrough();
+
+      const { file } = fileSchema.parse(req.file);
+
+      res.json({ message: "Ok" });
+    } catch (error) {
+      throw error;
+    }
+    res.json({ file: req.file });
   }
 }
 
